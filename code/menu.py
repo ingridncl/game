73cfc23189116a631e0db47fc3dbe0650823c0a7
ingridncl <ingrid_ncl@hotmail.com>
2025-08-08 15:11:56 -0300

    Adding classes generated from StarUML

---
+++ code/menu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Menu import Menu
+
+
+class Menu(Menu):
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+
+    def run(self, ):
+        pass
